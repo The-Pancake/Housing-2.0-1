5852597838d2fb562666f9d743ca21e7515f57e6
hed <hed@rpi.edu>
2023-04-07 18:16:29 -0400

    Simple fix

--- my_backend/my_test.py
+++ my_backend/my_test.py
@@ -7,17 +7,24 @@ import json
 import unittest                 # Testing framework
 
 # In progress 
-class Test_Ideal_Search(unittest.TestCase):
 
+class Test_Ideal_Search(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls): 
+        print("Setting up...")
+    
+    ''' Set up before running tests'''
+    def setUp(self):
+        file = open("/home/patty/my_projects/rcos/housing_project/Housing-2.0-B/single_student_input.json")
+        self.student = json.load(file)
+        file.close()    
+        
     ''' Testing Ideal Search for single student '''
     def test_single_ideal_search_out(self): 
         # Testing if output is correct
         # Opening file and passing it into single_ideal_search 
-        file = open("/home/patty/my_projects/rcos/housing_project/Housing-2.0-B/single_student_input.json")
-        student = json.load(file)
-        file.close()
-        
-        testOutput = my_ideal_search.single_ideal_search(student)
+
+        testOutput = my_ideal_search.single_ideal_search(self.student)
         self.assertEqual(testOutput, ["JohnSmith", 7, "Warren1-1"])
 
     ''' Testing if adding student works ''' 
@@ -29,7 +36,7 @@ class Test_Ideal_Search(unittest.TestCase):
         file.close()
         
         # See if student has been added 
-        self.assertEqual(my_add_student.add_student(student), "true")
+        self.assertEqual(my_add_student.add_student(self.student), "true")
         # Compare modified json after student has been added 
         self.assertEqual()        
     
